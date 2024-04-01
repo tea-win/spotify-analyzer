abb1b0fdc719d67fa6c4e931040478f3d8b9df3d
tea-win <47162766+tea-win@users.noreply.github.com>
2024-04-01 23:43:01 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // config.js
 
-export const CLIENT_ID = "8257910114464efd830d39b82a375ad4";
-export const CLIENT_SECRET = "d903f41e8c9148bbb3400f0776d141d5";
+export const CLIENT_ID = "add_your_here";
+export const CLIENT_SECRET = "add_your_here";
