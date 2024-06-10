aba04e7727be2778a8d5b110d51863f0f41b602e
tea-win <47162766+tea-win@users.noreply.github.com>
2024-06-09 20:46:07 -0700

    added create-grid func

--- src/App.js
+++ src/App.js
@@ -1,111 +1,114 @@
 import React, { useState, useEffect } from "react";
 import "./index.css";
 import { CLIENT_ID, CLIENT_SECRET } from "./config";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import TracksPage from "./TracksPage";
-import { Link } from "react-router-dom";
+import ImageGrid from "./ImageGrid";
 
-function App() {
+const App = () => {
   const [searchInput, setSearchInput] = useState("");
   const [accessToken, setAccessToken] = useState("");
   const [albums, setAlbums] = useState([]);
+  const [savedCovers, setSavedCovers] = useState([]); // State to store saved album covers
+  const [savedAlbumIds, setSavedAlbumIds] = useState([]); // State to store IDs of saved albums
+
+  // Debounce function
+  const debounce = (func, delay) => {
+    let timer;
+    return function (...args) {
+      clearTimeout(timer);
+      timer = setTimeout(() => func(...args), delay);
+    };
+  };
 
   // Function to fetch albums and tracks
   const fetchAlbumsAndTracks = async () => {
     if (!accessToken || !searchInput) return;
 
-    // Fetch albums
-    var artistID = await fetch(
-      "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
+    // Fetch artist ID
+    const response = await fetch(
+      `https://api.spotify.com/v1/search?q=${searchInput}&type=artist`,
       {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
-          Authorization: "Bearer " + accessToken,
+          Authorization: `Bearer ${accessToken}`,
         },
       }
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        return data.artists.items[0].id;
-      });
+    );
+    const data = await response.json();
+    const artistID = data.artists.items[0].id;
 
-    var returnedAlbums = await fetch(
-      "https://api.spotify.com/v1/artists/" +
-        artistID +
-        "/albums" +
-        "?include_groups=album,single&market=US&limit=50",
+    // Fetch albums
+    const albumsResponse = await fetch(
+      `https://api.spotify.com/v1/artists/${artistID}/albums?include_groups=album,single&market=US&limit=50`,
       {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
-          Authorization: "Bearer " + accessToken,
+          Authorization: `Bearer ${accessToken}`,
         },
       }
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const albumsWithInfo = data.items.map((album) => ({
-          ...album,
-          type: album.album_type === "album" ? "Album" : "Single",
-          releaseYear: parseInt(album.release_date.slice(0, 4)),
-        }));
-        setAlbums(albumsWithInfo);
-        return albumsWithInfo;
-      });
+    );
+    const albumsData = await albumsResponse.json();
+    const albumsWithInfo = albumsData.items.map((album) => ({
+      ...album,
+      type: album.album_type === "album" ? "Album" : "Single",
+      releaseYear: parseInt(album.release_date.slice(0, 4)),
+    }));
+    setAlbums(albumsWithInfo);
 
     // Fetch tracks for each album
-    returnedAlbums.forEach(async (album) => {
-      const tracks = await fetch(
+    const updatedAlbums = [];
+    for (const album of albumsWithInfo) {
+      const tracksResponse = await fetch(
         `https://api.spotify.com/v1/albums/${album.id}/tracks`,
         {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
-            Authorization: "Bearer " + accessToken,
+            Authorization: `Bearer ${accessToken}`,
           },
         }
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          return data.items.map((track) => track.name);
-        });
-
-      // Update state with tracks for the current album
-      setAlbums((prevAlbums) => {
-        return prevAlbums.map((prevAlbum) => {
-          if (prevAlbum.id === album.id) {
-            return { ...prevAlbum, tracks };
-          }
-          return prevAlbum;
-        });
-      });
-    });
+      );
+      const tracksData = await tracksResponse.json();
+      const tracks = tracksData.items.map((track) => track.name);
+      updatedAlbums.push({ ...album, tracks });
+    }
+    setAlbums(updatedAlbums);
   };
 
   useEffect(() => {
-    var authParameters = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body:
-        "grant_type=client_credentials&client_id=" +
-        CLIENT_ID +
-        "&client_secret=" +
-        CLIENT_SECRET,
+    const fetchAccessToken = async () => {
+      const authParameters = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`,
+      };
+      const response = await fetch(
+        "https://accounts.spotify.com/api/token",
+        authParameters
+      );
+      const data = await response.json();
+      setAccessToken(data.access_token);
     };
-    fetch("https://accounts.spotify.com/api/token", authParameters)
-      .then((result) => result.json())
-      .then((data) => setAccessToken(data.access_token));
+    fetchAccessToken();
   }, []);
 
-  useEffect(() => {
-    fetchAlbumsAndTracks();
-  }, [accessToken, searchInput]);
+  const handleSearch = (event) => {
+    if (
+      event.type === "click" ||
+      (event.type === "keydown" && event.key === "Enter")
+    ) {
+      fetchAlbumsAndTracks();
+    }
+  };
 
-  const handleSearch = () => {
-    fetchAlbumsAndTracks();
+  const handleSaveImage = async (albumId, imageUrl) => {
+    setSavedCovers((prevCovers) => [...prevCovers, imageUrl]);
+    setSavedAlbumIds((prevIds) => [...prevIds, albumId]);
   };
 
   return (
@@ -123,8 +126,12 @@ function App() {
                     placeholder="Search for Artist"
                     type="input"
                     onChange={(event) => setSearchInput(event.target.value)}
+                    onKeyDown={handleSearch}
                   />
                   <button onClick={handleSearch}>Search</button>
+                  <Link to="/create-grid" className="grid-link">
+                    <button>Create Grid</button>
+                  </Link>
                 </div>
                 <div className="albums-container">
                   {albums.map((album, i) => (
@@ -138,18 +145,18 @@ function App() {
                           <h3>{album.name}</h3>
                           <p>Release Year: {album.releaseYear}</p>
                           <p>Type: {album.type}</p>
-                          {/* {album.tracks && (
-                            <div>
-                              <p>Name:</p>
-                              <ul>
-                                {album.tracks.map((track, index) => (
-                                  <li key={index}>{track}</li>
-                                ))}
-                              </ul>
-                            </div>
-                          )} */}
                         </div>
                       </Link>
+                      <button
+                        onClick={() =>
+                          handleSaveImage(album.id, album.images[0].url)
+                        }
+                        disabled={savedAlbumIds.includes(album.id)}
+                      >
+                        {savedAlbumIds.includes(album.id)
+                          ? "Saved"
+                          : "Save Cover"}
+                      </button>
                     </div>
                   ))}
                 </div>
@@ -160,10 +167,14 @@ function App() {
             path="/:albumId/tracks"
             element={<TracksPage accessToken={accessToken} />}
           />
+          <Route
+            path="/create-grid"
+            element={<ImageGrid savedCovers={savedCovers} />}
+          />
         </Routes>
       </Router>
     </div>
   );
-}
+};
 
 export default App;
